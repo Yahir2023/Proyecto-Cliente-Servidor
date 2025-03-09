caac5a8348a442a05d575197f763a86cf2e3d9b0
Cristian Chi <202300087@upqroo.edu.mx>
2025-03-09 18:22:24 -0500

    cambios asientos

--- routes/asientos.js
+++ routes/asientos.js
@@ -1,45 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const mysql = require('mysql');
-const dotenv = require("dotenv");
-dotenv.config();
-
-// Configuración de la base de datos
-let connection;
-
-try {
-    connection = mysql.createConnection({
-        host: process.env.DBHOST,
-        user: process.env.DBUSER,
-        password: process.env.DBPASS,
-        database: process.env.DBNAME
-    });
-} catch (error) {
-    console.log("Error al conectar con la base de datos");
-}
-
-// Middleware para verificar token JWT para la ruta de asientos
-const defaultMiddleware = (req, res, next) => {
-    const token = req.header("Authorization");
-    if (!token) return res.status(401).json({ mensaje: "Acceso denegado" });
-
-    try {
-        const verificado = jwt.verify(token, process.env.SECRET_KEY || "secreto");
-        req.usuario = verificado;
-
-        // Verificar si el usuario tiene el rol adecuado (por ejemplo, 'admin')
-        if (req.usuario.rol !== 'Gerente') {
-            return res.status(403).json({ mensaje: "Acceso prohibido: solo administradores" });
-        }
-
-        next();
-    } catch (err) {
-        res.status(400).json({ mensaje: "Token no válido" });
-    }
-};
+const { connection } = require("../config/config.db"); // Importamos la conexión desde config.db
 
 // Obtener todos los asientos
-router.get('/asientos', defaultMiddleware, (req, res) => {
+router.get('/asientos', (req, res) => {
     const query = 'SELECT * FROM asientos';
     connection.query(query, (err, results) => {
         if (err) {
@@ -50,14 +14,14 @@ router.get('/asientos', defaultMiddleware, (req, res) => {
 });
 
 // Agregar un nuevo asiento
-router.post('/asientos', defaultMiddleware, (req, res) => {
+router.post('/asientos', (req, res) => {
     const { id_sala, numero_asiento, fila, tipo_asiento } = req.body;
 
     const query = `
         INSERT INTO asientos (id_sala, numero_asiento, fila, tipo_asiento, created_at, updated_at)
         VALUES (?, ?, ?, ?, NOW(), NOW());
     `;
-    
+
     connection.query(query, [id_sala, numero_asiento, fila, tipo_asiento], (err, result) => {
         if (err) {
             console.error('Error al agregar el asiento:', err);
@@ -68,7 +32,7 @@ router.post('/asientos', defaultMiddleware, (req, res) => {
 });
 
 // Eliminar un asiento
-router.delete('/asientos/:id', defaultMiddleware, (req, res) => {
+router.delete('/asientos/:id', (req, res) => {
     const id = req.params.id;
     const query = 'DELETE FROM asientos WHERE id_asiento = ?';
     
