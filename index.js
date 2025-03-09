417164716de127a3dbecf3354890ee785fef926e
Yahir2023 <nohramirezalejandro@gmail.com>
2025-03-09 13:44:30 -0500

    Cabios en orden de rutas

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const path = require("path");
-const dotenv = require("dotenv").config();
-
 const app = express();
 
-// Middlewares para parsear JSON y datos URL-encoded
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Servir archivos estáticos desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Rutas existentes
@@ -17,16 +13,8 @@ app.use(require('./routes/Pagos'));
 app.use(require('./routes/Peliculas'));
 app.use(require('./routes/Promociones'));
 app.use(require('./routes/admin'));
-
-// Rutas API adicionales
-const authRoutes = require("./routes/Login");
-app.use("/api", authRoutes);
-
-const reservasRoutes = require("./routes/Reservas");
-app.use("/api", reservasRoutes);
-
-//const movieRoutes = require("./routes/Lt_Peliculas");
-//app.use("/api", movieRoutes);
+app.use(require('./routes/Login'));
+app.use(require('./routes/Reservas'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
